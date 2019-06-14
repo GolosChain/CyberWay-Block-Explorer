4d7ddac17d3c075618f96b19b3ae28ad3de44c4f
Ilya Lebedev <bacherko@gmail.com>
2019-06-14 16:44:58 +0300

    feat: make skeleton

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <header>HEADER</header>
+      <main>MAIN</main>
+      <footer>FOOTER</footer>
+    </div>
+  );
+}
