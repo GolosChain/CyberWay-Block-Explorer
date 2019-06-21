05ec0f73ccf201c13edf84b187e0ec80bf50a8e3
Ilya Lebedev <bacherko@gmail.com>
2019-06-21 14:37:43 +0300

    st

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,20 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.GLS_FRONTEND_PORT || 3000;
 
-app.use(express.static('../build'));
+const BUILD_DIR = path.join(__dirname, '../../build');
+
+app.use(express.static(BUILD_DIR));
+
+// Отдаем index.html на все запросы за text/html
+app.use('/*', (req, res, next) => {
+  if (req.method === 'GET' && req.headers.accept.startsWith('text/html')) {
+    res.sendFile(path.join(BUILD_DIR, 'index.html'));
+  } else {
+    next();
+  }
+});
 
 app.listen(port, err => {
   if (err) {
