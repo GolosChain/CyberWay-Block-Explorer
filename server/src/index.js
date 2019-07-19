85c51b05120f10de6121b8383af1c27b2c408f32
Ilya Lebedev <bacherko@gmail.com>
2019-07-19 18:30:54 +0300

    feat: add api /trx/%id%.json

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,51 @@
 const path = require('path');
 const express = require('express');
+const jayson = require('jayson');
 const app = express();
 const port = process.env.GLS_FRONTEND_PORT || 3000;
 
 const BUILD_DIR = path.join(__dirname, '../../build');
 
 app.use(express.static(BUILD_DIR));
+
+const blocksClient = jayson.client.http(process.env.GLS_BLOCKS_CONNECT);
+
+function getTransaction(transactionId) {
+  return new Promise((resolve, reject) => {
+    blocksClient.request('getTransaction', { transactionId }, (err, response) => {
+      if (err) {
+        reject(err);
+      } else if (response.error) {
+        reject(response.error);
+      } else {
+        resolve(response.body);
+      }
+    });
+  });
+}
+
+app.get('/trx/:trxId.json', async (req, res) => {
+  try {
+    const trx = await getTransaction(req.params.trxId);
+
+    res.json(trx);
+  } catch (err) {
+    if (err.code === 404) {
+      res.status(404);
+      res.json({
+        error: 'Not found',
+      });
+      return;
+    }
+
+    console.error(new Date().toJSON(), 'Transaction fetch failed:', err);
+
+    res.status(500);
+    res.json({
+      error: 'Internal Server Error',
+    });
+  }
+});
 
 // Отдаем index.html на все запросы за text/html
 app.use('/*', (req, res, next) => {
