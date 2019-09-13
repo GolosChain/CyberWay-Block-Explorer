018730140617a3299a122ad4e4b1c2c233ff4642
Ilya Lebedev <bacherko@gmail.com>
2019-09-13 12:07:24 +0300

    fix: fix login with password (#12)
    * fix: fix login with password
    
    * version 0.3.3

--- src/utils/password.js
+++ src/utils/password.js
@@ -1,18 +1,12 @@
 import ecc from 'eosjs-ecc';
 
-export function getKey(accountId, privateKey, keyRole = 'active') {
+export function getKey({ accountId, golosId, userInput, keyRole = 'active' }) {
   try {
     // If key could be converted into public key it means key is valid
-    ecc.privateToPublic(privateKey, 'GLS');
-    return privateKey;
+    ecc.privateToPublic(userInput, 'GLS');
+    return userInput;
   } catch {
     // If it's not a key try extract key from seed
-    return passwordToKey(accountId, privateKey, keyRole);
+    return ecc.seedPrivate(`${golosId || accountId}${keyRole}${userInput}`);
   }
-}
-
-function passwordToKey(accountId, password, keyRole) {
-  const privateKey = ecc.seedPrivate(`${accountId}${keyRole}${password}`);
-  ecc.privateToPublic(privateKey, 'GLS');
-  return privateKey;
 }
